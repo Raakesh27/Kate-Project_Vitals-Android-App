5610fa01235d73c81c365b2247266f4feae5cfa2
Wiehan <boshoff.2@wright.edu>
2017-02-01 17:08:52 -0500

    New files added

---
+++ Kate_Vitals_App/app/src/main/java/com/example/wiehan/kate_vitals_app/Activity_two.java
@@ -0,0 +1,20 @@
+package com.example.wiehan.kate_vitals_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Activity_two extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+
+
+
+    }
+
+
+
+    }
+
